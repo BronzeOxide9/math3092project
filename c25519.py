77a9940afeff065c6bd1f2a3f23d7549d5ab4d03
BronzeOxide9 <102553076+BronzeOxide9@users.noreply.github.com>
2024-05-07 11:14:45 +0100

    Add files via upload
    Python Serial communication program

---
+++ c25519.py
@@ -0,0 +1,32 @@
+import ctypes
+import serial
+import time
+
+libcurve25519 = ctypes.CDLL('./libcurve25519.so')
+
+libcurve25519.generate_keypair.argtypes = [ctypes.POINTER(ctypes.c_ubyte), ctypes.POINTER(ctypes.c_ubyte)]
+libcurve25519.x25519.argtypes = [ctypes.POINTER(ctypes.c_ubyte), ctypes.POINTER(ctypes.c_ubyte), ctypes.POINTER(ctypes.c_ubyte)]
+libcurve25519.x25519.restype = None
+
+def to_ctypes_array(bytes_obj):
+    return (ctypes.c_ubyte * len(bytes_obj))(*bytes_obj)
+
+ser = serial.Serial('/dev/cu.usbmodem1401', 9600)
+time.sleep(2) 
+
+B_public = (ctypes.c_ubyte * 32)()
+B_private = (ctypes.c_ubyte * 32)()
+libcurve25519.generate_keypair(B_public, B_private)
+
+A_public_bytes = ser.read(32)
+A_public = to_ctypes_array(A_public_bytes)
+
+ser.write(bytes(A_public))
+
+shared_secret = (ctypes.c_ubyte * 32)()
+libcurve25519.x25519(shared_secret, A_public, B_private)
+
+shared_secret_bytes = bytes(bytearray(shared_secret))
+print("The key is:", shared_secret_bytes.hex())
+
+ser.close()
